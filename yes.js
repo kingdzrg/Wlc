81331c15f86372b216e8f01416c4f2c5ae29a964
kingdzrg <58010386+kingdzrg@users.noreply.github.com>
2019-12-07 14:38:34 +0100

    Update yes.js

--- yes.js
+++ yes.js
@@ -1,11 +1,48 @@
 const Discord = require('discord.js'),
     client = new Discord.Client({sisableEveryone: true})
     console.log("دوز دوز,");
+
 client.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("545651706893959221");
+const mohamed= member.guild.channels.get("651481399139893267");
 if(!mohamed) return;
 if(mohamed) {
 setTimeout(() => mohamed.send(`**Welcome 2 Geem. :yes:**`), 4000)        
 }
 });
+
+const devs = ["537675177677291530"];
+const adminprefix = ["-"];
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+
+  if (message.content.startsWith(prefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+     if (message.content === (prefix + "leave")) {
+    message.guild.leave();
+  } else
+  if (message.content.startsWith(prefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(prefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(prefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(prefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(prefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
 client.login(process.env.TOKEN);
