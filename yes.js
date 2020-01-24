78f45895c43e7cb43ad9afa4d7a90cfc44f384c4
kingdzrg <58010386+kingdzrg@users.noreply.github.com>
2020-01-24 18:55:04 +0100

    Update yes.js

--- yes.js
+++ yes.js
@@ -5,7 +5,7 @@ client.on('guildMemberAdd', member => {
 const mohamed= member.guild.channels.get("663095128013144084");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`**Hey, Welcome To Trust ! :Zine1:**`), 4000)        
+setTimeout(() => mohamed.send(`**Hey, Welcome To Trust !** <:Zine1:670079102874812417> `), 4000)        
 }
 });
 
