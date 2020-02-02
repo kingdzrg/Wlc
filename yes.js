fd418ad9b1bcef546b24a27d249ba133543be4c7
kingdzrg <58010386+kingdzrg@users.noreply.github.com>
2020-02-02 23:49:54 +0100

    Update yes.js

--- yes.js
+++ yes.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("673257832115732494");
+const mohamed= member.guild.channels.get("673647025224679424");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`** > Welcome To Trust ! .. Check #・أثبت_نفسك :cherry_blossom: :notes: **`), 4000)        
+setTimeout(() => mohamed.send(`**Welcome To Trust ! .. Check <#673631347016859665> <a:emoji_41:673447060963590166> <a:love:673487121247502377>**`), 4000)        
 }
 });
 
